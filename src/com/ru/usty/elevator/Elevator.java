959fb19cd5bf4a3b95144213065553cf6a2c493a
asgeiro14 <asgeir14@ru.is>
2018-02-24 11:59:42 +0000

    elevator stuff

--- src/com/ru/usty/elevator/Elevator.java
+++ src/com/ru/usty/elevator/Elevator.java
@@ -1,7 +1,8 @@
 package com.ru.usty.elevator;
 
 public class Elevator {
-	// 1.
-	// 2. 
-	// 3. 
+	// 1. Get number of people in elevator
+	// 2. if people waiting on floor > 0 && people in elevator < capacity : let people in elevator 
+	// 3. Switch floor
+	// 4. Let people out of elevator
 }
