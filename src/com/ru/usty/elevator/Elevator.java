8cbdc8493cb1904968dd869346c3dea3bd469c8e
asgeiro14 <asgeir14@ru.is>
2018-02-24 11:40:37 +0000

    Elevator class

---
+++ src/com/ru/usty/elevator/Elevator.java
@@ -0,0 +1,7 @@
+package com.ru.usty.elevator;
+
+public class Elevator {
+	// 1.
+	// 2. 
+	// 3. 
+}
--- src/com/ru/usty/elevator/Person.java
+++ src/com/ru/usty/elevator/Person.java
@@ -5,6 +5,7 @@ public class Person implements Runnable {
 
 	int sourceFloor, destinationFloor;
 	private ElevatorScene scene;
+	
 	public Person(int sourceFloor, int destinationFloor) {
 		this.sourceFloor = sourceFloor;
 		this.destinationFloor = destinationFloor;
@@ -13,6 +14,13 @@ public class Person implements Runnable {
 	
 	@Override
 	public void run() {
+		// 1. increment waiting on floor
+		// 2. wait for elevator
+		// 3. increment people elevator 
+        // 4. decrement waiting on floor
+		// 5. wait to get out
+		// 6. decrement people in elevator
+
 		//ElevatorScene.elevatorWaitMUTEX = new Semaphore(1);
 		try {
 			ElevatorScene.elevatorWaitMUTEX.acquire(); 
